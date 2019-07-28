a076b0a0cd654d050aaf246d23a9f370cac4b3e7
Shivakumar M <mshivakumar.rymec@gmail.com>
2019-07-28 23:12:22 +0530

    intro with json values

--- player.js
+++ player.js
@@ -1,1 +1,18 @@
-//ADD YOUR CODE HERE.
+let jsonData = {};
+
+function jsonCallback(json){
+  jsonData = json;
+}
+
+$.ajax({
+  url: "guide.json",
+  jsonp: "jsonCallback",
+  dataType: "jsonp",
+  crossDomain: true
+});
+$(function(){
+	var introguide = introJs();
+	introguide.setOptions(jsonData);
+	introguide.setOption('positionPrecedence', ['left', 'right', 'top', 'bottom']);
+	introguide.start();
+});
